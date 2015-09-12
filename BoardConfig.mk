fecb80154003fb192ef20653dc25df945af733eb
sub77 <sub77@ymail.com>
2015-09-11 21:30:39 -0400

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,79 +1,127 @@
-USE_CAMERA_STUB := true
+# Assert
+# TARGET_OTA_ASSERT_DEVICE := degaswifi
 
-# inherit from the proprietary version
-#-include vendor/samsung/degaswifi/BoardConfigVendor.mk
-#-include device/samsung/lt02wifi/BoardConfig.mk
+BOARD_VENDOR := samsung
+
+# Architecture
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := mrvl 
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_BOARD_PLATFORM := mrvl
+TARGET_BOOTLOADER_BOARD_NAME := PXA1088
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a9
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
-TARGET_BOOTLOADER_BOARD_NAME := degaswifi
-TARGET_RECOVERY_FSTAB := device/samsung/degaswifi/recovery.fstab
+# Flags
+COMMON_GLOBAL_CFLAGS += -DMRVL_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
-BOARD_KERNEL_CMDLINE := 
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
+BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/degaswifi
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
-BOARD_HAS_NO_SELECT_BUTTON := true
+# Graphics
+USE_OPENGL_RENDERER := true
+BOARD_USE_BGRA_8888 := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
-BOARD_CUSTOM_MKBOOTIMG := device/samsung/degaswifi/degas-mkbootimg
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/degaswifi/custom_mkbootimg.mk
-BOARD_MKBOOTIMG_ARGS := --dt device/samsung/degaswifi/dt.img
-#TARGET_PREBUILT_KERNEL := device/samsung/degaswifi/kernel
+# Charging mode
+BOARD_CHARGING_MODE_BOOTING_LPM := "/sys/class/power_supply/battery/batt_lp_charging"
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
+BOARD_CHARGING_CMDLINE_VALUE := "charger"
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+
+# Override healthd HAL
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.mrvl
+
+# Target info
+USE_CAMERA_STUB := true
+
+# MRVL hardware
+BOARD_USES_MRVL_HARDWARE := true
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/degaswifi/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_MRVL := true
+
+# Graphics
+BOARD_USES_MRVL_HARDWARE := true
+BOARD_HAVE_PIXEL_FORMAT_INFO := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/samsung/degaswifi/configs/egl.cfg
+ENABLE_HWC_GC_PATH := true
+
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/degaswifi
-#TARGET_KERNEL_CONFIG := pxa1088_degaswifi_eur_defconfig
-#TARGET_KERNEL_VARIANT_CONFIG := pxa1088_degaswifi_eur_tgalal
-
-#twrp
-DEVICE_RESOLUTION := 800x1280
-RECOVERY_SDCARD_ON_DATA := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_NO_USB_STORAGE := true
-#TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-
-#cwm
+TARGET_KERNEL_CONFIG := pxa1088_degaswifi_eur_defconfig
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/degaswifi/degaswifi_mkbootimg.mk
+BOARD_MKBOOTIMG_ARGS := --dt device/samsung/degaswifi/rootdir/boot.img-dt --ramdisk_offset 0x01000000
+BOARD_KERNEL_CMDLINE := initrd=0x01100000,1m rw androidboot.console=ttyS1 console=ttyS1,115200 panic_debug
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/degaswifi/include
+    
+# Partitions
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 12582912
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2224029696
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5230297088
+BOARD_FLASH_BLOCK_SIZE := 4096
+BLOCK_BASED_OTA := false
 
-#Generic config
-MRVL_ION := true
-# The above lines are almost the same as Brownstone.
-# MMP3 Special
-BOARD_USE_VIVANTE_GRALLOC := true
-HDMI_SUPPORT_3D := true
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/degaswifi/recovery.fstab
+BOARD_SUPPRESS_EMMC_WIPE := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+BOARD_SUPPRESS_SECURE_ERASE := true
 
-#BOARD_GFX_DRIVER_VERSION=4x
+#TWRP
+#RECOVERY_VARIANT := twrp
+TW_CUSTOM_THEME := device/samsung/degaswifi/recovery/twres
+#TW_THEME := portrait_hdpi
+TW_DEFAULT_EXTERNAL_STORAGE := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
+#TW_NEW_ION_HEAP := true
+TW_SCREEN_BLANK_ON_BOOT := true
 
-#DYNAMIC_ALSA_PARAMS := true
 
-#Enable marvell interface in SurfaceFlinger
-MRVL_INTERFACE_ANIMATION := true
-#ENABLE_HWC_GC_PATH := true
+# Vold
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
+BOARD_UMS_LUNFILE := "/sys/devices/platform/soc.2/d4200000.axi/mv-udc/gadget/lun0/file"
 
-#Launch DMS in SurfaceFlinger process
-MRVL_LAUNCH_DMS_IN_SURFACEFLINGER := true
+# WiFi
+BOARD_WLAN_VENDOR := MRVL
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/sd8xxx.ko"
+WIFI_DRIVER_MODULE_NAME	:= "sd8xxx"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/firmware/mrvl/sd8777_uapsta.bin cfg80211_wext=12 sta_name=wlan uap_name=wlan wfd_name=p2p fw_name=mrvl/sd8777_uapsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/sd8xxx/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/mrvl/sd8777_uapsta.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/firmware/mrvl/sd8777_uapsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/firmware/mrvl/sd8777_uapsta.bin"
+WIFI_SDIO_IF_DRIVER_MODULE_PATH := "/system/lib/modules/mlan.ko"
+WIFI_SDIO_IF_DRIVER_MODULE_NAME := "mlan"
+WIFI_SDIO_IF_DRIVER_MODULE_ARG := ""
+MRVL_WIRELESS_DAEMON_API := true
 
-TARGET_FORCE_CPU_UPLOAD := true
-
-# Init
-TARGET_PROVIDES_INIT := true
-TARGET_PROVIDES_INIT_TARGET_RC := true
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/degaswifi
