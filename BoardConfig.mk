02ac89a16dd93ace441c834dfc4df024bd1ddea7
sub77 <sub77@ymail.com>
2015-09-11 23:57:33 -0400

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,12 +33,16 @@ BOARD_USE_BGRA_8888 := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 # Charging mode
-BOARD_CHARGING_MODE_BOOTING_LPM := "/sys/class/power_supply/battery/batt_lp_charging"
-BOARD_BATTERY_DEVICE_NAME := "battery"
-BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
-BOARD_CHARGING_CMDLINE_VALUE := "charger"
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_CHARGING_MODE_BOOTING_LPM := true
+
+# Charging mode
+#BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+#BOARD_CHARGING_MODE_BOOTING_LPM := true
+#BOARD_BATTERY_DEVICE_NAME := "battery"
+#BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
+#BOARD_CHARGING_CMDLINE_VALUE := "charger"
+#BOARD_CHARGER_ENABLE_SUSPEND := true
+#BOARD_CHARGER_SHOW_PERCENTAGE := true
 
 
 # Override healthd HAL
@@ -67,8 +71,9 @@ BOARD_EGL_CFG := device/samsung/degaswifi/configs/egl.cfg
 ENABLE_HWC_GC_PATH := true
 
 # Kernel
+TARGET_PREBUILT_KERNEL := device/samsung/degaswifi
 TARGET_KERNEL_SOURCE := kernel/samsung/degaswifi
-TARGET_KERNEL_CONFIG := pxa1088_degaswifi_eur_defconfig
+TARGET_KERNEL_CONFIG := twrp_degaswifi_defconfig
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/degaswifi/degaswifi_mkbootimg.mk
 BOARD_MKBOOTIMG_ARGS := --dt device/samsung/degaswifi/rootdir/boot.img-dt --ramdisk_offset 0x01000000
@@ -90,6 +95,8 @@ TARGET_RECOVERY_FSTAB := device/samsung/degaswifi/recovery.fstab
 BOARD_SUPPRESS_EMMC_WIPE := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 BOARD_SUPPRESS_SECURE_ERASE := true
+TARGET_RECOVERY_INITRC := device/samsung/degaswifi/recovery/init.rc
+
 
 #TWRP
 #RECOVERY_VARIANT := twrp
@@ -101,7 +108,8 @@ RECOVERY_SDCARD_ON_DATA := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_NO_REBOOT_BOOTLOADER := true
 #TW_NEW_ION_HEAP := true
-TW_SCREEN_BLANK_ON_BOOT := true
+#TW_SCREEN_BLANK_ON_BOOT := true
+TW_HAS_MTP := true
 
 
 # Vold
--- device.mk
+++ device.mk
@@ -1,18 +1,24 @@
 LOCAL_PATH := device/samsung/degaswifi
 
+# Charger
+PRODUCT_PACKAGES += charger charger_res_images
+
+# ADB BOOT
+ADDITIONAL_DEFAULT_PROPERTIES += \
+   persist.sys.usb.config=mass_storage,mtp
+
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
-#    $(LOCAL_PATH)/recovery/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
-
-
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/kernel:kernel \
-    $(LOCAL_PATH)/dt.img:dt.img \
-    $(LOCAL_PATH)/recovery/sbin/healthd:root/sbin/healthd
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/recovery/sbin/lpm:root/sbin/lpm
+
+#    $(LOCAL_PATH)/recovery/sbin/healthd:root/sbin/healthd
 
 $(call inherit-product, build/target/product/full.mk)
 
